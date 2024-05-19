133ac440462c6e8c7e033c309374d336b1973dd3
unknown <gesfkhdghkm@protonmail.com>
2024-05-20 02:32:53 +0530

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 6969,
-  },
+    plugins: [react()],
+    base: "/vite-deploy/",
+    server: {
+        port: 6969,
+    },
 });
